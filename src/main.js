15f022baa32b82c7687f8e251e58d7157ff46480
chuwei <chuw@ngarihealth.com>
2019-12-17 17:56:59 +0800

    登录页修改

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
 
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8443/api'
@@ -13,11 +14,27 @@ Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {
+    if (store.state.user.username) {
+      next()
+    } else {
+      next({
+        path: 'login',
+        query: {redirect: to.fullPath}
+      })
+    }
+  } else {
+    next()
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App),
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,41 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
-
+import Home from '@/components/Home'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+      meta: {
+        requireAuth: true
+      }
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      // home页面并不需要被访问
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'HelloWorld',
+          component: HelloWorld,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+    },
     {
       path: '/login',
       name: 'Login',
       component: Login
-    },
-    {
-      path: '/index',
-      name: 'HelloWorld',
-      component: HelloWorld
     }
   ]
 })
